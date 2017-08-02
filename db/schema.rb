a66963991e7a599738c2d47f3df144c1011ee156
Ohto Takeshi <boxing.kingdom.11@gmail.com>
2017-08-02 13:22:19 +0000

    new-column

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170802114940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "value"
+    t.integer "value"
   end
 
 end
